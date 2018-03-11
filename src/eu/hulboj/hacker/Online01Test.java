537bf9c439d8fc1724d805de3207a57f5a7a6772
Radoslaw Hulboj <radekhulboj@gmil.com>
2018-03-11 15:02:22 +0100

    Update codility test

--- src/eu/hulboj/hacker/Online01Test.java
+++ src/eu/hulboj/hacker/Online01Test.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static java.util.regex.Pattern.matches;
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 /**
  * Created by radoslaw on 10.03.18.
@@ -44,5 +46,16 @@ public class Online01Test {
         int solution1 = solution.solution(ints);
         //then
         assertTrue(solution1 == 5);
+    }
+    @Test
+    public void useCase4() throws Exception {
+        //given
+        Online01 solution = new Online01();
+        List<Integer> integers = Arrays.asList(456, 45, 667,7,8,8 );
+        int[] ints = integers.stream().mapToInt((Integer value) -> value.intValue()).toArray();
+        //when
+        int solution1 = solution.solution(ints);
+        //then
+        assertThat(solution1, is(9));
     }
 }
